cc6e9310f1f0fddf0308f6b0350716839b4ae7e2
razherana <razherana@gmail.com>
2024-08-12 01:04:42 +0300

    Added mysqlconnection exception

--- src/framework/components/database/connection/MySqlConnection.php
+++ src/framework/components/database/connection/MySqlConnection.php
@@ -3,7 +3,7 @@
 namespace framework\components\database\connection;
 
 use framework\components\database\Database;
-
+use framework\components\database\exceptions\MysqlConnectionException;
 use mysqli;
 
 class MySqlConnection extends mysqli
@@ -22,13 +22,13 @@ class MySqlConnection extends mysqli
   {
     static::$self = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, !is_null($database) ? $database->read_config('port') : null);
     if (static::$self == false)
-      throw new \Exception("Connection to database failed", 1);
+      throw new MysqlConnectionException("Connection to database failed");
   }
 
   public static function get()
   {
     if (is_null(static::$self)) {
-      throw new \Exception("The database connection isn't initialized", 1);
+      throw new MysqlConnectionException("The database connection isn't initialized");
     }
     return static::$self;
   }
---
+++ src/framework/components/database/exceptions/MysqlConnectionException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace framework\components\database\exceptions;
+
+use framework\base\exceptions\DefaultException;
+
+/**
+ * Default exception of mysql connection
+ */
+class MysqlConnectionException extends DefaultException
+{
+}
