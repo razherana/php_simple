bcadb825a46efb905b5589d6c267b4727198d23c
razherana <razherana@gmail.com>
2024-08-14 00:06:27 +0300

    Added is_query for parentheses fix

--- src/framework/components/database/orm/mysql/request/elements/From.php
+++ src/framework/components/database/orm/mysql/request/elements/From.php
@@ -12,12 +12,17 @@ class From implements MysqlElement
    * Contains the from element
    * @var string $from
    */
-  private $from = '';
+  public $from = '';
+
+  /**
+   * If the subquery is a MysqlQueryable
+   */
+  protected $is_query = false;
 
   /**
    * Contains the alias
    */
-  private $as = null;
+  public $as = null;
 
   /**
    * @param string|MysqlQueryable $from
@@ -27,6 +32,7 @@ class From implements MysqlElement
   {
     if ($from instanceof MysqlQueryable) {
       $from = $from->decode_query();
+      $this->is_query = true;
       if (!is_string($as)) throw new QueryException("AS is required when using a sub-query for FROM \n`" . $from . "`");
     }
     $this->from = $from;
@@ -35,6 +41,13 @@ class From implements MysqlElement
 
   public function decode(): string
   {
-    return 'FROM (' . $this->from . ')' . (is_null($this->as) ? '' : (" AS " . $this->as));
+    $as = "";
+    if (!is_null($this->as)) $as = ' AS ' . $this->as;
+
+    $from = $this->from;
+    // If FROM is a subquery, add parentheses
+    if ($this->is_query) $from = "($from)";
+
+    return "FROM " . $from . $as;
   }
 }
