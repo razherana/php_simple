6a6b2f20320dddcb447274cf0d0c527c5422da1a
razherana <razherana@gmail.com>
2024-08-14 11:44:29 +0300

    Added autoloader config Added multiple sources for the autoloader

---
+++ config/autoloader.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Contains the autoloader config
+ */
+return [
+  /**
+   * Contains the directory's of classes to load
+   */
+  "sources" => [
+    // Default directory
+    "src"
+  ]
+];
--- src/autoloader.php
+++ src/autoloader.php
@@ -4,8 +4,15 @@ use framework\base\exceptions\ClassNotFoundException;
 
 spl_autoload_register(function ($className) {
   $classPath = str_replace('\\', '/', $className);
-  if (file_exists($fileName = ___DIR___ . '/src/' . $classPath . ".php")) {
-    return require_once($fileName);
-  }
+
+  $sources = include(___DIR___ . '/config/autoloader.php');
+
+  $sources = $sources['sources'] ?? [];
+
+  foreach ($sources as $source)
+    if (file_exists($fileName = ___DIR___ . "/$source/" . $classPath . ".php")) {
+      return require_once($fileName);
+    }
+
   throw new ClassNotFoundException($className);
 });
