b537bf845b03e14cfbba61c5437c190755ea58e9
razherana <razherana@gmail.com>
2024-08-14 12:01:21 +0300

    Did some more test in the test_file

--- resources/tests/test.php
+++ resources/tests/test.php
@@ -2,4 +2,15 @@
 
 use framework\components\database\orm\mysql\queries\SortedQueryMaker;
 
-dd(SortedQueryMaker::where('test.id', '=', 6)->select()->and_where('users.id', '!=', null)->from('test')->join('users')->on(fn () => $this->where('users.id', '!=', null))->decode_query());
+dd(SortedQueryMaker::where('test.id', '=', 6)
+  ->select()
+  ->from('test')
+  ->and_group_where(function () {
+    $this->where('herana', 'LIKE', '%herana%')
+      ->or_where('test', '>', 6)
+      ->and_where('herana', '!=', 'herana');
+  })
+  ->and_where('users.id', '!=', null)
+  ->join('users')
+  ->on(fn () => $this->where('users.id', '!=', null))
+  ->decode_query());
