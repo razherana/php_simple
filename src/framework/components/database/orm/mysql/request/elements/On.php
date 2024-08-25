d6ed1af2d67ab242bd5a04c9459abeb220e8807c
razherana <razherana@gmail.com>
2024-08-25 18:43:59 +0300

    Did more safe unsetting in On

--- src/framework/components/database/orm/mysql/request/elements/On.php
+++ src/framework/components/database/orm/mysql/request/elements/On.php
@@ -2,7 +2,9 @@
 
 namespace framework\components\database\orm\mysql\request\elements;
 
+use ErrorException;
 use framework\components\database\orm\mysql\exceptions\MysqlJoinException;
+use framework\components\database\orm\mysql\queries\DefaultQueryMaker;
 use framework\components\database\orm\mysql\request\interfaces\MysqlElement;
 use framework\components\database\orm\mysql\request\MysqlQueryable;
 
@@ -23,23 +25,26 @@ class On implements MysqlElement
    */
   public function __construct($condition_callable, $mysql_queryable_class)
   {
-    if (is_a($mysql_queryable_class, MysqlQueryable::class)) {
-      throw new MysqlJoinException("The mysql_queryable_class : '$mysql_queryable_class' given is not a MysqlQueryable object");
-    }
 
     // Creates an object of a mysql_queryable
-    $mysql_queryable = new $mysql_queryable_class();
+    $mysql_queryable = new DefaultQueryMaker();
     $mysql_queryable->mode_test = true;
 
     // Bind to object and call it
-    $condition_callable->bindTo($mysql_queryable, $mysql_queryable_class)();
+    $condition_callable->call($mysql_queryable);
 
     // Take the first element of the new object (where) and set it to property
-    $this->wheres = $mysql_queryable->elements;
+    $wheres = $mysql_queryable->elements;
 
     // Removes first 6 characters "WHERE "
-    if (count($this->wheres) > 0)
-      unset($this->wheres[0]->data[0]);
+    if (count($wheres) > 0) {
+      $data = $wheres[0]->data;
+      unset($data[0]);
+
+      $wheres[0]->data = $data;
+    }
+
+    $this->wheres = $wheres;
   }
 
   public function decode(): string
