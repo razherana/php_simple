0c523700bc55a6e22dddd7b76ba5472527ef9021
razherana <razherana@gmail.com>
2024-08-25 18:46:12 +0300

    Added the function add_select in SelectTrait

--- src/framework/components/database/orm/mysql/traits/SelectTrait.php
+++ src/framework/components/database/orm/mysql/traits/SelectTrait.php
@@ -2,6 +2,7 @@
 
 namespace framework\components\database\orm\mysql\traits;
 
+use framework\components\database\orm\mysql\exceptions\MysqlSelectException;
 use framework\components\database\orm\mysql\request\elements\Select;
 
 /**
@@ -13,20 +14,38 @@ trait SelectTrait
   use MysqlRequestTrait;
 
   /**
-   * @param string[] $elements
+   * @param array<int|string, string>|string $elements
    */
-  protected static function select_static($elements = ['*'])
+  protected static function select_static($elements = ['*'], $use_model_name_prefix = true)
   {
     $el = new static();
-    return $el->select_instance($elements);
+    return $el->select_instance($elements, $use_model_name_prefix);
   }
 
   /**
-   * @param string[] $elements
+   * @param array<int|string, string>|string $elements
    */
-  protected function select_instance($elements = ['*'])
+  protected function select_instance($elements = ['*'], $use_model_name_prefix = true)
   {
-    $this->elements[] = new Select($elements);
+    $this->elements[] = new Select($elements, $use_model_name_prefix);
+    return $this;
+  }
+
+  /**
+   * @param array<int|string, string>|string $elements
+   */
+  public function add_select($elements = [], $use_model_name_prefix = true)
+  {
+    /**
+     * @var false|Select $select
+     */
+    $select = $this->search_element(Select::class);
+
+    if ($select === false)
+      throw new MysqlSelectException("Cannot add_select because there is no Select mysql element");
+
+    $select->add_select($elements, $use_model_name_prefix);
+
     return $this;
   }
 }
