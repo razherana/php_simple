817d3dcb973140dfe5fe4e001c71461fe6c34166
razherana <razherana@gmail.com>
2024-08-25 19:00:01 +0300

    Added more tests

--- resources/tests/test.php
+++ resources/tests/test.php
@@ -1,16 +1,75 @@
 <?php
 
-use framework\components\database\orm\mysql\queries\SortedQueryMaker;
+use framework\components\database\orm\mysql\models\BaseModel;
 
-dd(SortedQueryMaker::where('test.id', '=', 6)
-  ->select()
-  ->from('test')
-  ->and_group_where(function () {
-    $this->where('herana', 'LIKE', '%herana%')
-      ->or_where('test', '>', 6)
-      ->and_where('herana', '!=', 'herana');
-  })
-  ->and_where('users.id', '!=', null)
-  ->join('users')
-  ->on(fn () => $this->where('users.id', '!=', null))
-  ->decode_query());
+class User extends BaseModel
+{
+  public static $table = 'users';
+
+  public static $with = [];
+
+  public static $primary_key = 'id';
+
+  public function messages_sent()
+  {
+    return $this->has_many(Message::class, 'id', 'id_sender', function () {
+      $this->where('id_sender', '<', 5);
+    });
+  }
+
+  public function messages_received()
+  {
+    return $this->has_many(Message::class, 'id', 'id_receiver');
+  }
+}
+
+class Message extends BaseModel
+{
+  public static $table = 'messages';
+
+  public static $primary_key = 'id';
+
+  public static $with = ['reactions'];
+
+  public function reactions()
+  {
+    return $this->has_many(Reaction::class, 'id', 'id_message');
+  }
+
+  public function user_sender()
+  {
+    return $this->belongs_to(User::class, 'id_sender', 'id');
+  }
+
+  public function user_receiver()
+  {
+    return $this->belongs_to(User::class, 'id_receiver', 'id');
+  }
+}
+
+class Reaction extends BaseModel
+{
+  public static $table = 'reactions';
+
+  public static $with = [];
+
+  public function user()
+  {
+    return $this->belongs_to(User::class, 'id_user', 'id');
+  }
+
+  public function message()
+  {
+    return $this->belongs_to(Message::class, 'id_message', 'id');
+  }
+}
+
+// $u = User::with(['posts'])->relation('posts2', function () {
+//   return $this->has_many(Post::class, 'id', 'id2');
+// });
+// $u->get();
+
+$a = User::with(['messages_sent']);
+dd($a->get());
+
+// dd($u->relation_info_maps);
