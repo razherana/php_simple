b4b810e2cd08e8a02018b3ff82fc20f0204a733e
razherana <razherana@gmail.com>
2024-09-01 01:31:35 +0300

    Separated RelationAccess in ModelInstance

--- src/framework/components/database/orm/mysql/models/instances/ModelInstance.php
+++ src/framework/components/database/orm/mysql/models/instances/ModelInstance.php
@@ -4,21 +4,17 @@ namespace framework\components\database\orm\mysql\models\instances;
 
 use framework\components\database\orm\mysql\models\BaseModel;
 use framework\components\database\orm\mysql\models\instances\traits\ModelInstanceTrait;
+use framework\components\database\orm\mysql\models\instances\traits\ModelRelationAccessTrait;
 
 class ModelInstance extends DefaultModelInstance
 {
-  use ModelInstanceTrait;
+  use ModelInstanceTrait, ModelRelationAccessTrait;
 
   /**
    * Contains Model::class or Model if possible
    * @var BaseModel|string $parent_model
    */
   public $parent_model;
-
-  /**
-   * Contains relation elements
-   */
-  public $relations = [];
 
   /**
    * @param array $attributes
@@ -28,5 +24,17 @@ class ModelInstance extends DefaultModelInstance
   {
     parent::__construct($attributes);
     $this->parent_model = $parent_model;
+  }
+
+  /**
+   * Main __get magic method
+   */
+  public function __get($name)
+  {
+    // Get the relation
+    if (($relation = $this->relation_access($name)) !== false) {
+      return $relation;
+    }
+    return parent::__get($name);
   }
 }
