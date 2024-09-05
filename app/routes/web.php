2aa403719ac5127c67ecadd68b7123634e18dbe7
razherana <razherana@gmail.com>
2024-09-05 11:37:16 +0300

    Added some tests in web

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
 use framework\components\route\Route;
+use framework\rule\Rule;
 
 Route::get('/', function () {
-  return "test" . ($this->request->getParameters['test'] ?? '');
+  $this->plain();
+  
+  return '<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    
+  </body>
+  </html>';
 })->name('index')->save();
 
 Route::get('/<<var>>/<<var2>>', function ($var, $var2) {
   return "test " . ($var) . " aiza koa " . ($var2) . "<br>";
-})->name('index.var')->save();
+})->name('index.var')->rules([
+  Rule::from('var')->number()
+])->save();
