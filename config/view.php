fc3fbe2f79e5cd7b23aabab5e43c8e568be31505
razherana <razherana@gmail.com>
2024-09-06 14:56:13 +0300

    Added view config

---
+++ config/view.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Contains all config for views
+ */
+return [
+
+  /**
+   * Contains the folder of views
+   */
+  "folder" => "resources/views",
+
+  /**
+   * Contains the folder for compiled views
+   */
+  "compiled" => "storage/views",
+
+  /**
+   * Contains the map file for the compiled views
+   */
+  "map" => "storage/map/views",
+
+  /**
+   * Tells if we always compile in every request
+   */
+  "always_compile" => true
+];
