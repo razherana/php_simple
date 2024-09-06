1247fac562aac33bf23226b80a51b11a84e6b49c
razherana <razherana@gmail.com>
2024-09-06 14:59:47 +0300

    Removed the dollar signs

--- src/framework/base/config/exceptions/UnknownConfigException.php
+++ src/framework/base/config/exceptions/UnknownConfigException.php
@@ -8,9 +8,9 @@ class UnknownConfigException extends ConfigException
 
   public function __construct($config_name, $config_file, $config_full_path)
   {
-    $this->$config_name = $config_name;
-    $this->$config_file = $config_file;
-    $this->$config_full_path = $config_full_path;
+    $this->config_name = $config_name;
+    $this->config_file = $config_file;
+    $this->config_full_path = $config_full_path;
 
     parent::__construct("The config :'$config_name' in '$config_file' with a full path : '$config_full_path' doesn't exist");
   }
