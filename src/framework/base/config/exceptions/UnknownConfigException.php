f51bb680b7a8c8f78789144846b7f19de2ab5e3e
razherana <razherana@gmail.com>
2024-09-07 20:31:27 +0300

    Changed the exception message in UnknownConfig

--- src/framework/base/config/exceptions/UnknownConfigException.php
+++ src/framework/base/config/exceptions/UnknownConfigException.php
@@ -12,6 +12,6 @@ class UnknownConfigException extends ConfigException
     $this->config_file = $config_file;
     $this->config_full_path = $config_full_path;
 
-    parent::__construct("The config :'$config_name' in '$config_file' with a full path : '$config_full_path' doesn't exist");
+    parent::__construct("The config '$config_name' in '$config_file' with a full path : '$config_full_path' doesn't exist");
   }
 }
