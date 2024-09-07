81884eb34e9c2cee59bc47308a5e930cd61d1ba5
razherana <razherana@gmail.com>
2024-09-07 20:31:56 +0300

    Added data in view function helper

--- src/framework/functions/ViewFunctions.php
+++ src/framework/functions/ViewFunctions.php
@@ -2,6 +2,7 @@
 
 use compilers\php\PhpCompiler;
 use framework\view\comm\ViewElement;
+use framework\view\comm\ViewVars;
 use framework\view\compiler\Compilation;
 
 /**
@@ -10,10 +11,11 @@ use framework\view\compiler\Compilation;
  * @param string $compiler
  * @return ViewElement Returns the compiled ViewElement
  */
-function view($view_name, $compiler = PhpCompiler::class)
+function view($view_name, $data = [], $compiler = PhpCompiler::class)
 {
   // Creates the view element
   $view_el = new ViewElement($view_name);
+  $view_el->vars = new ViewVars($data);
 
   // Creates the compiler
   $compiler = new $compiler($view_el);
