a9f3c96704fd58c4dee6ae1beadcad0c2675409a
razherana <razherana@gmail.com>
2024-09-08 20:40:47 +0300

    Added $elements as args in view function

--- src/framework/functions/ViewFunctions.php
+++ src/framework/functions/ViewFunctions.php
@@ -7,15 +7,16 @@ use framework\view\compiler\Compilation;
 
 /**
  * This function is a fast helper to get a view element with it's content
- * @param string $view_name
- * @param string $compiler
+ * @param string $view_name The view's name
+ * @param string $compiler The compiler to use
+ * @param array $elements Elements to use (block, templates, ...)
  * @return ViewElement Returns the compiled ViewElement
  */
-function view($view_name, $data = [], $compiler = PhpCompiler::class)
+function view($view_name, $data = [], $compiler = PhpCompiler::class, $elements = [])
 {
   // Creates the view element
   $view_el = new ViewElement($view_name);
-  $view_el->vars = new ViewVars($data);
+  $view_el->vars = new ViewVars($data, $elements);
 
   // Creates the compiler
   $compiler = new $compiler($view_el);
