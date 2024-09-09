7d9c48e8eb2d17ae758092ef6b0393f1ebc3dbfa
razherana <razherana@gmail.com>
2024-09-09 20:59:08 +0300

    Added exception on unknown database type

--- src/framework/components/database/Database.php
+++ src/framework/components/database/Database.php
@@ -5,6 +5,7 @@ namespace framework\components\database;
 use framework\base\Component;
 use framework\base\config\ConfigurableElement;
 use framework\components\database\connection\MySqlConnection;
+use framework\components\database\exceptions\DatabaseException;
 
 class Database extends ConfigurableElement implements Component
 {
@@ -18,8 +19,12 @@ class Database extends ConfigurableElement implements Component
     /**
      * Connect to database
      */
-    if ($this->read_cached_config('type') == 'mysql') {
-      MySqlConnection::initialize($this);
+    switch ($type = $this->read_cached_config('type')) {
+      case 'mysql':
+        MySqlConnection::initialize($this);
+        break;
+      default:
+        throw new DatabaseException("This database type is not supported : $type");
     }
   }
 
