30ba63fb95b58dc4a1b980ba09b06dbd8803107f
razherana <razherana@gmail.com>
2024-09-12 01:34:34 +0300

    Added session_initialize in Auth Added name in attribute and __construct Added login method Added logout method Added register method Added force_logins Added protected save method

--- src/framework/components/database/auth/Auth.php
+++ src/framework/components/database/auth/Auth.php
@@ -4,12 +4,23 @@ namespace framework\components\database\auth;
 
 use framework\base\config\ConfigurableElement;
 use framework\components\database\auth\exceptions\AuthDefinitionException;
+use framework\components\database\auth\exceptions\AuthException;
 use framework\components\database\orm\mysql\models\BaseModel;
+use framework\components\database\orm\mysql\models\instances\ModelInstance;
+use framework\components\database\orm\mysql\queries\DefaultQueryMaker;
+use framework\components\database\orm\mysql\request\elements\Where;
+use framework\components\session\interfaces\SessionInitializeInterface;
 use framework\components\session\SessionManager;
 use framework\components\session\interfaces\SessionReservedKeywordsInterface;
 
-class Auth extends ConfigurableElement implements SessionReservedKeywordsInterface
+class Auth extends ConfigurableElement implements SessionReservedKeywordsInterface, SessionInitializeInterface
 {
+  /**
+   * Contains the name of this Auth
+   * @var string $name
+   */
+  protected $name = '';
+
   /**
    * Contains the model to use
    * @var string $model
@@ -58,23 +69,34 @@ class Auth extends ConfigurableElement implements SessionReservedKeywordsInterfa
     ];
   }
 
+  public static function initialize_session(): void
+  {
+    $key = (new Auth)->read_config('session_key_name');
+    $session = new SessionManager(true);
+
+    if (!$session->exists($key))
+      !$session->set($key, []);
+  }
+
   /**
    * Constructs a new Auth
-   * @param string $model The Model::class, if $mode === "ignore" it skips checkings and settings
+   * @param string $name The auth's name, if $name === "ignore" it skips checkings and settings
+   * @param string $model The Model::class, if $model === "ignore" it skips checkings and settings
    * @param string[]|string $id_columns
    * @param ?string $pass_column
    * @param ?\Closure $hash_method
    * @param ?\Closure $password_verify
    * @return self
    */
-  public function __construct($model = "ignore", $id_columns = null, $pass_column = null, $hash_method = null, $password_verify = null)
+  public function __construct($name = "ignore", $model = "ignore", $id_columns = null, $pass_column = null, $hash_method = null, $password_verify = null)
   {
-    if ($model === "ignore") return;
+    if ($model === "ignore" || $name === "ignore") return;
 
     if (!is_a($model, BaseModel::class, true))
       throw new AuthDefinitionException("This class is not a model : " . $model, $this);
 
-    $this->session = new SessionManager;
+    $this->name = $name;
+    $this->session = new SessionManager(true);
     $this->model = $model;
 
     if (is_string($id_columns))
@@ -102,13 +124,192 @@ class Auth extends ConfigurableElement implements SessionReservedKeywordsInterfa
     $auths = (new Auth)->read_config('auths');
 
     if (!isset($auths[$name_of_auth]))
-      throw new AuthDefinitionException("This auth doesn't exist in config : $name_of_auth");
+      throw new AuthDefinitionException("This auth doesn't exist in config : '$name_of_auth'");
 
     $config = $auths[$name_of_auth];
 
-    return new Auth(...$config);
+    return new Auth($name_of_auth, ...$config);
   }
 
   // Auth functions starts here
 
+  /**
+   * Login using datas
+   * @param array{id_column: array<string, string>, pass_column: string} $data
+   */
+  public function login($data): bool
+  {
+    $id = $data['id_column'];
+
+    $id_columns_and = $this->id_columns;
+
+    foreach ($id_columns_and as $_id) if (!isset($id[$_id]))
+      throw new AuthException("The id_column passed for Auth doesn't have the obligatory id : '$_id'", $this);
+
+    $obligatory_id = array_filter($id, function ($k) use ($id_columns_and) {
+      return in_array($k, $id_columns_and);
+    }, ARRAY_FILTER_USE_KEY);
+
+    $non_obligatory_id = array_filter($id, function ($k) use ($id_columns_and) {
+      return !in_array($k, $id_columns_and);
+    }, ARRAY_FILTER_USE_KEY);
+
+    $pass_column = $this->pass_column;
+
+    $model = $this->model;
+
+    /** @var BaseModel $query */
+    $query = new $model(true, false, false);
+
+    // Repair obligatory_id to match where_all
+    $ob_id = [];
+    foreach ($obligatory_id as $k => $v)
+      $ob_id[] = [$k, $v];
+
+    $non_ob_id = [];
+    foreach ($non_obligatory_id as $k => $v)
+      $non_ob_id[] = [$k, $v];
+
+
+    // Add the wheres in query
+    if (empty($non_obligatory_id)) {
+      // Match the where_all()
+      $query->where_all($ob_id);
+    } else {
+      $query
+        ->where_all($non_ob_id, Where::OR)
+        ->and_group_where(function () use ($ob_id) {
+          /** @var DefaultQueryMaker $this */
+          $this->where_all($ob_id);
+        });
+    }
+
+    /** @var ModelInstance[] $result */
+    $result = $query->get();
+
+    if (count($result) != 1)
+      return false;
+
+    $result = $result[array_keys($result)[0]];
+
+    if (is_null($pass_column))
+      return $this->save($result);
+
+    if (!$result->offsetExists($pass_column))
+      throw new AuthException("The pass_column : '$pass_column' doesn't exist in the model given -> " . var_export($result, true), $this);
+
+    $hashed_password = $result->{$pass_column};
+    $password = $data['pass_column'];
+
+    if (($this->password_verify)($password, $hashed_password))
+      return $this->save($result);
+
+    return false;
+  }
+
+  /**
+   * Removes the login of the auth
+   * @return bool
+   */
+  public function logout()
+  {
+    $auth = $this->session->get($this->read_cached_config("session_key_name"));
+    unset($auth[$this->name]);
+
+    return $this->session->set($this->read_cached_config("session_key_name"), $auth);
+  }
+
+  /**
+   * Register a new element with password hashed
+   * @param array<string, string> $data
+   * @return bool
+   */
+  public function register($data)
+  {
+    // Hash the pass column
+    if (!is_null($this->pass_column)) {
+      if (!isset($data[$this->pass_column]))
+        throw new AuthException("The data given in register doesn't have a pass column, but the pass column is required", $this);
+
+      $data[$this->pass_column] = ($this->hash_method)($data[$this->pass_column]);
+    }
+
+    return ($this->model)::create($data);
+  }
+
+  /**
+   * Force login with an id
+   * @param mixed $id
+   */
+  public function force_login_id($id)
+  {
+    $auth = $this->session->get($this->read_cached_config("session_key_name"));
+    $auth[$this->name] = $id;
+
+    return $this->session->set($this->read_cached_config("session_key_name"), $auth);
+  }
+
+  /**
+   * Force login with id_columns
+   * @param array<string, string> $id_columns
+   */
+  public function force_login($id_columns)
+  {
+    $id = $id_columns;
+
+    $id_columns_and = $this->id_columns;
+
+    foreach ($id_columns_and as $_id) if (!isset($id[$_id]))
+      throw new AuthException("The id_column passed for Auth doesn't have the obligatory id : '$_id'", $this);
+
+    $obligatory_id = array_filter($id, function ($k) use ($id_columns_and) {
+      return in_array($k, $id_columns_and);
+    }, ARRAY_FILTER_USE_KEY);
+
+    $non_obligatory_id = array_filter($id, function ($k) use ($id_columns_and) {
+      return !in_array($k, $id_columns_and);
+    }, ARRAY_FILTER_USE_KEY);
+
+    $model = $this->model;
+
+    /** @var BaseModel $query */
+    $query = new $model(true, true, false);
+
+    // Add the wheres in query
+    if (empty($non_obligatory_id)) {
+      $query->where_all($obligatory_id);
+    } else {
+      $query
+        ->where_all($non_obligatory_id, Where::OR)
+        ->and_group_where(function () use ($obligatory_id) {
+          /** @var DefaultQueryMaker $this */
+          $this->where_all($obligatory_id);
+        });
+    }
+
+    /** @var ModelInstance[] $result */
+    $result = $query->get();
+
+    if (count($result) != 1)
+      return false;
+
+    $result = $result[array_keys($result)[0]];
+
+    return $this->save($result);
+  }
+
+  /**
+   * Saves the $model_instance into the auth session
+   * @param ModelInstance $model_instance The authenticated model
+   * @return bool
+   */
+  protected function save($model_instance)
+  {
+    $key = $model_instance[$model_instance->parent_model::$primary_key];
+
+    $auth = $this->session->get($this->read_cached_config("session_key_name"));
+    $auth[$this->name] = $key;
+
+    return $this->session->set($this->read_cached_config("session_key_name"), $auth);
+  }
 }
