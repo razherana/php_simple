37ca1036ac0cb65611fef43af0c562d93eb11933
razherana <razherana@gmail.com>
2024-09-11 14:17:18 +0300

    Added more methods Added throws Added reserved_keywords

--- src/framework/components/session/SessionManager.php
+++ src/framework/components/session/SessionManager.php
@@ -3,9 +3,17 @@
 namespace framework\components\session;
 
 use framework\components\session\exceptions\SessionException;
+use framework\components\session\interfaces\SessionInitialize;
+use framework\components\session\interfaces\SessionReservedKeywordsInterface;
 
-class SessionManager
+class SessionManager implements SessionReservedKeywordsInterface, SessionInitialize
 {
+  /**
+   * If Admin mode is set to true, then no reserved keyword checking is done
+   * @var bool $admin_mode
+   */
+  protected $admin_mode = false;
+
   /**
    * Contains the content of $_SESSION
    * @var array $session_data
@@ -17,27 +25,73 @@ class SessionManager
    * We use a static var so every Component can add their own reserved keys
    * @var array $RESERVED_KEYS
    */
-  public static $RESERVED_KEYS = [
-    "___temp___"
-  ];
+  public static $RESERVED_KEYS = [];
 
   /**
    * Construct a new SessionManager
    */
-  public function __construct()
+  public function __construct($admin_mode = false)
   {
     $this->session_data = &$_SESSION;
+    $this->admin_mode = $admin_mode;
+  }
+
+  public static function get_session_reserved_keywords(): array
+  {
+    return [
+      "___temp___",
+      "___tempdelete___"
+    ];
+  }
+
+  public static function initialize_session(): void
+  {
+    $session = new self(true);
+
+    if (!$session->exists("___temp___")) {
+      $session->set("___temp___", []);
+      return;
+    }
+
+    if (!$session->exists("___tempdelete___")) {
+      $session->set("___tempdelete___", []);
+      return;
+    }
+
+    $tempdelete = $session->get('___tempdelete___');
+
+    foreach ($tempdelete as $_tempdel) if ($session->temp_exists($_tempdel))
+      $session->temp_destroy($_tempdel);
+
+    $session->set("___tempdelete___", array_keys($session->get('___temp___')));
+  }
+
+  /**
+   * Regenerates the id of the session
+   */
+  public static function regenerate($delete_old_session = true): bool
+  {
+    if (session_status() !== PHP_SESSION_ACTIVE)
+      throw new SessionException("No session active, cannot regenerate");
+    if (headers_sent())
+      throw new SessionException("Headers have already been sent, cannot regenerate");
+
+    return session_regenerate_id($delete_old_session);
   }
 
   /**
    * Remove the key from Session
+   * If the key is reserved, an exception is thrown
+   * If the key doesn't exist, an exception is thrown
    * @param string $key
-   * @return bool
+   * @return bool If the key is destroyed, then __TRUE__ else __FALSE__
    */
   public function destroy($key): bool
   {
-    if (in_array($key, $this::$RESERVED_KEYS))
+    if (!$this->admin_mode && in_array($key, $this::$RESERVED_KEYS))
       throw new SessionException("The key : $key is reserved");
+    if (!$this->exists($key))
+      throw new SessionException("The key : $key doesn't exist");
 
     unset($this->session_data[$key]);
     return !$this->exists($key);
@@ -46,15 +100,15 @@ class SessionManager
   /**
    * Sets a value to session
    * @param string $key
-   * @param mixed &$value
+   * @param mixed $value
    * @return bool
    */
-  public function set($key, &$value): bool
+  public function set($key, $value): bool
   {
-    if (in_array($key, $this::$RESERVED_KEYS))
+    if (!$this->admin_mode && in_array($key, $this::$RESERVED_KEYS))
       throw new SessionException("The key : $key is reserved");
 
-    $this->session_data[$key] = &$value;
+    $this->session_data[$key] = $value;
     return $this->exists($key) && $this->session_data[$key] === $value;
   }
 
@@ -62,10 +116,10 @@ class SessionManager
    * Sets a temporary value to session
    * It means that this value would be deleted after another request
    * @param string $key
-   * @param mixed &$value
+   * @param mixed $value
    * @return bool
    */
-  public function temp($key, &$value): bool
+  public function temp_set($key, $value): bool
   {
     $this->session_data['___temp___'][$key] = &$value;
     return $this->temp_exists($key) && $this->session_data['___temp___'][$key] === $value;
@@ -76,7 +130,7 @@ class SessionManager
    * @param string $key
    * @return bool
    */
-  public function exists($key)
+  public function exists($key): bool
   {
     return in_array($key, array_keys($this->session_data ?? []));
   }
@@ -86,7 +140,7 @@ class SessionManager
    * @param string $key
    * @return bool
    */
-  public function temp_exists($key)
+  public function temp_exists($key): bool
   {
     return $this->exists("___temp___")
       && in_array($key, array_keys($this->session_data['___temp___'] ?? []));
@@ -98,7 +152,7 @@ class SessionManager
    * @param string $key
    * @return mixed 
    */
-  public function get($key)
+  public function get($key): mixed
   {
     if (!$this->exists($key))
       throw new SessionException("This $key doesn't exist in session");
@@ -112,11 +166,26 @@ class SessionManager
    * @param string $key
    * @return mixed 
    */
-  public function temp_get($key)
+  public function temp_get($key): mixed
   {
     if (!$this->temp_exists($key))
       throw new SessionException("This $key doesn't exist in temp session");
 
     return $this->session_data['___temp___'][$key];
+  }
+
+  /**
+   * Destroy a temp value in session
+   * If it doesn't exist, an exception is thrown
+   * @param string $key
+   * @return bool If the temp key is destroyed, then __TRUE__ else __FALSE__
+   */
+  public function temp_destroy($key): bool
+  {
+    if (!$this->temp_exists($key))
+      throw new SessionException("The temp key : $key doesn't exist");
+
+    unset($this->session_data["___temp___"][$key]);
+    return !$this->temp_exists($key);
   }
 }
