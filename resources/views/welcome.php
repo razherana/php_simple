0e2731dbfd6eb89d93f42eaf6f8004e82821355e
razherana <razherana@gmail.com>
2024-09-16 22:30:54 +0300

    Added welcome.php for demo

---
+++ resources/views/welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome</title>
+</head>
+
+<body>
+  <h1>Welcome to simple_php, where building your app becomes easy</h1>
+  <h2>Go to <a href="<?= route('hphp.index', ['testvalue' => 'Herana']) ?>">HPHP file</a></h2>
+</body>
+
+</html>
