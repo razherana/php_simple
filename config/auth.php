877bcaaf402fd9c8b5c339055756807fa62b4e14
razherana <razherana@gmail.com>
2024-10-04 22:51:26 +0300

    Removed User test model

--- config/auth.php
+++ config/auth.php
@@ -13,7 +13,6 @@ return [
    */
   "auths" => [
     // "name_of_auth" => [Model::class, 'id_column'|['id_column1', 'id_column2', ...], 'pass_column', ?\Closure hash_method, ?\Closure password_verify, ?\Closure id_relation],
-    "users" => [User::class, 'name']
   ],
 
   /**
